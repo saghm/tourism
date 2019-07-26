f25bb155d8dbcf7220a093335fa104b294dfbf97
Saghm Rossi <saghmrossi@gmail.com>
2019-07-26 00:21:33 -0400

    implement traits for primitives and collections as needed

--- tourism/src/visit.rs
+++ tourism/src/visit.rs
@@ -1,4 +1,7 @@
-use std::iter::FromIterator;
+use std::{
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque},
+    hash::Hash,
+};
 
 use super::Visitor;
 
@@ -13,13 +16,121 @@ where
     }
 }
 
-impl<I, T, W> Visit<W> for I
+macro_rules! impl_noop_visit {
+    ( $ty1:ty, $( $ty2:ty ),+ ) => {
+        impl_noop_visit!($ty1);
+        impl_noop_visit!( $( $ty2 ),+ );
+    };
+
+    ( $ty:ty ) => {
+            impl<W> Visit<W> for $ty {}
+    };
+}
+
+impl_noop_visit!(
+    (),
+    bool,
+    char,
+    f32,
+    f64,
+    i8,
+    i16,
+    i32,
+    i64,
+    i128,
+    isize,
+    &str,
+    String,
+    u8,
+    u16,
+    u32,
+    u64,
+    u128,
+    usize
+);
+
+impl<T, W> Visit<W> for Vec<T>
 where
-    I: IntoIterator<Item = T> + FromIterator<T>,
-    T: Visit<W> + Sized,
     W: Visitor<T>,
+    T: Visit<W>,
 {
     fn walk(self, visitor: &mut W) -> Self {
         self.into_iter().map(|t| visitor.visit(t)).collect()
+    }
+}
+
+impl<T, W> Visit<W> for BTreeSet<T>
+where
+    W: Visitor<T>,
+    T: Ord + Visit<W>,
+{
+    fn walk(self, visitor: &mut W) -> Self {
+        self.into_iter().map(|t| visitor.visit(t)).collect()
+    }
+}
+
+impl<T, W> Visit<W> for BinaryHeap<T>
+where
+    W: Visitor<T>,
+    T: Ord + Visit<W>,
+{
+    fn walk(self, visitor: &mut W) -> Self {
+        self.into_iter().map(|t| visitor.visit(t)).collect()
+    }
+}
+
+impl<T, W> Visit<W> for HashSet<T>
+where
+    W: Visitor<T>,
+    T: Hash + Eq + Visit<W>,
+{
+    fn walk(self, visitor: &mut W) -> Self {
+        self.into_iter().map(|t| visitor.visit(t)).collect()
+    }
+}
+
+impl<T, W> Visit<W> for LinkedList<T>
+where
+    W: Visitor<T>,
+    T: Visit<W>,
+{
+    fn walk(self, visitor: &mut W) -> Self {
+        self.into_iter().map(|t| visitor.visit(t)).collect()
+    }
+}
+
+impl<T, W> Visit<W> for VecDeque<T>
+where
+    W: Visitor<T>,
+    T: Visit<W>,
+{
+    fn walk(self, visitor: &mut W) -> Self {
+        self.into_iter().map(|t| visitor.visit(t)).collect()
+    }
+}
+
+impl<K, V, W> Visit<W> for BTreeMap<K, V>
+where
+    W: Visitor<K> + Visitor<V>,
+    K: Ord + Visit<W>,
+    V: Visit<W>,
+{
+    fn walk(self, visitor: &mut W) -> Self {
+        self.into_iter()
+            .map(|(k, v)| (visitor.visit(k), visitor.visit(v)))
+            .collect()
+    }
+}
+
+impl<K, V, W> Visit<W> for HashMap<K, V>
+where
+    W: Visitor<K> + Visitor<V>,
+    K: Hash + Eq + Visit<W>,
+    V: Visit<W>,
+{
+    fn walk(self, visitor: &mut W) -> Self {
+        self.into_iter()
+            .map(|(k, v)| (visitor.visit(k), visitor.visit(v)))
+            .collect()
     }
 }
--- tourism/src/visitor.rs
+++ tourism/src/visitor.rs
@@ -1,23 +1,136 @@
-use std::iter::FromIterator;
+use std::{
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque},
+    hash::Hash,
+};
 
 use super::Visit;
 
-pub trait Visitor<W>
+pub trait Visitor<U>
 where
-    W: Visit<Self> + Sized,
+    U: Visit<Self>,
 {
-    fn visit(&mut self, visit: W) -> W {
+    fn visit(&mut self, visit: U) -> U {
         visit.walk(self)
     }
 }
 
-impl<I, T, U> Visitor<I> for U
+macro_rules! impl_noop_visitor {
+    ( $ty1:ty, $( $ty2:ty ),+ ) => {
+        impl_noop_visitor!($ty1);
+        impl_noop_visitor!( $( $ty2 ),+ );
+    };
+
+    ( $ty:ty ) => {
+         impl<U> Visitor<$ty> for U {}
+    };
+}
+
+impl_noop_visitor!(
+    (),
+    bool,
+    char,
+    f32,
+    f64,
+    i8,
+    i16,
+    i32,
+    i64,
+    i128,
+    isize,
+    &str,
+    String,
+    u8,
+    u16,
+    u32,
+    u64,
+    u128,
+    usize
+);
+
+impl<T, U> Visitor<Vec<U>> for T
 where
-    U: Visitor<T>,
-    I: IntoIterator<Item = T> + FromIterator<T>,
-    T: Visit<U> + Sized,
+    T: Visitor<U>,
+    U: Visit<Self>,
 {
-    fn visit(&mut self, visit: I) -> I {
-        visit.walk(self)
+    fn visit(&mut self, visit: Vec<U>) -> Vec<U> {
+        visit.into_iter().map(|t| t.walk(self)).collect()
+    }
+}
+
+impl<T, U> Visitor<BTreeSet<U>> for T
+where
+    T: Visitor<U>,
+    U: Ord + Visit<Self>,
+{
+    fn visit(&mut self, visit: BTreeSet<U>) -> BTreeSet<U> {
+        visit.into_iter().map(|t| t.walk(self)).collect()
+    }
+}
+
+impl<T, U> Visitor<BinaryHeap<U>> for T
+where
+    T: Visitor<U>,
+    U: Ord + Visit<Self>,
+{
+    fn visit(&mut self, visit: BinaryHeap<U>) -> BinaryHeap<U> {
+        visit.into_iter().map(|t| t.walk(self)).collect()
+    }
+}
+
+impl<T, U> Visitor<HashSet<U>> for T
+where
+    T: Visitor<U>,
+    U: Hash + Eq + Visit<Self>,
+{
+    fn visit(&mut self, visit: HashSet<U>) -> HashSet<U> {
+        visit.into_iter().map(|t| t.walk(self)).collect()
+    }
+}
+
+impl<T, V> Visitor<LinkedList<T>> for V
+where
+    V: Visitor<T>,
+    T: Visit<Self>,
+{
+    fn visit(&mut self, visit: LinkedList<T>) -> LinkedList<T> {
+        visit.into_iter().map(|t| t.walk(self)).collect()
+    }
+}
+
+impl<T, V> Visitor<VecDeque<T>> for V
+where
+    V: Visitor<T>,
+    T: Visit<Self>,
+{
+    fn visit(&mut self, visit: VecDeque<T>) -> VecDeque<T> {
+        visit.into_iter().map(|t| t.walk(self)).collect()
+    }
+}
+
+impl<K, V, T> Visitor<BTreeMap<K, V>> for T
+where
+    T: Visitor<K> + Visitor<V>,
+    K: Ord + Visit<Self>,
+    V: Visit<Self>,
+{
+    fn visit(&mut self, visit: BTreeMap<K, V>) -> BTreeMap<K, V> {
+        visit
+            .into_iter()
+            .map(|(k, v)| (k.walk(self), v.walk(self)))
+            .collect()
+    }
+}
+
+impl<K, V, T> Visitor<HashMap<K, V>> for T
+where
+    T: Visitor<K> + Visitor<V>,
+    K: Hash + Eq + Visit<Self>,
+    V: Visit<Self>,
+{
+    fn visit(&mut self, visit: HashMap<K, V>) -> HashMap<K, V> {
+        visit
+            .into_iter()
+            .map(|(k, v)| (k.walk(self), v.walk(self)))
+            .collect()
     }
 }
